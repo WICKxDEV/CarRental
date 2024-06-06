2e11ae0080af87a9ea86b0411233a169f4094208
WICKxDEV <isuruwickramasinghe.sliate@gmail.com>
2024-06-06 19:36:23 +0530

    Create frmLogin.cs

---
+++ frmLogin.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TMS
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if(MainClass.IsValidUser(txtUser.Text,txtPass.Text) == false )
+            {
+                guna2MessageDialog1.Show("invalid username or password");
+                return;
+            }
+            else
+            {
+                this.Hide();
+                frmMain frm = new frmMain();
+                frm.Show();
+            }
+        }
+    }
+}
